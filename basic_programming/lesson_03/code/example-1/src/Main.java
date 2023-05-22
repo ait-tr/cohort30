cf10c1f627916beec2b250f87cdfea0ad148f74d
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-05-22 22:02:26 +0200

    lesson update

--- basic_programming/lesson_03/code/example-1/src/Main.java
+++ basic_programming/lesson_03/code/example-1/src/Main.java
@@ -3,24 +3,24 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        System.out.println("Hello world!"); // это для вывода на консоль (на печать)
+        System.out.println("Добрый день!"); // это для вывода на консоль (на печать) приветствия
 
 //        Reader inputStreamReader = new InputStreamReader(System.in); // готовим ввод с клавиатуры
 //        BufferedReader bufferedReader = new BufferedReader(inputStreamReader); // готовим ввод с клавиатуры
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // вводимые значения попадвют в br
 
         System.out.println("Введите первое число: "); // Это приглашение к вводу
-        String num1 = br.readLine(); //читаем строку с клавиатуры
-        System.out.println("Вы ввели " + num1 + " - это первое число"); // Это соосбщение пользователю
+        String num1 = br.readLine(); //читаем строку с клавиатуры в переменную num1
+        System.out.println("Вы ввели " + num1 + " - это первое число"); // Это сообщение пользователю
         int n1 = Integer.parseInt(num1); //преобразовываем строку в число.
 
         System.out.println("Введите второе число: "); // Это приглашение к вводу
         String num2 = br.readLine(); //читаем строку с клавиатуры
-        System.out.println("Вы ввели " + num2 + " - это второе число"); // Это соосбщение пользователю
+        System.out.println("Вы ввели " + num2 + " - это второе число"); // Это сообщение пользователю
         int n2 = Integer.parseInt(num2); //преобразовываем строку в число.
 
-        System.out.println("1-е число " + n1 + " умножить на второе число " + n2 + " получилось " + (n1 * n2) );
+        System.out.println(" 1-е число  " + n1 + " умножить на второе число " + n2 + " получилось " + (n1 * n2) );
         System.out.println("Спасибо!");
     }
 }
