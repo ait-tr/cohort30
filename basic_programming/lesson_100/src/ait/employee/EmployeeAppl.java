1241f76c7a9b87f6429e2f536880ee1ad9e3b56e
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-06-06 18:56:51 +0200

    lesson update

---
+++ basic_programming/lesson_100/src/ait/employee/EmployeeAppl.java
@@ -0,0 +1,10 @@
+package ait.employee;
+
+import ait.employee.model.Employee;
+
+public class EmployeeAppl {
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+
+    }
+}
---
+++ basic_programming/lesson_100/src/ait/employee/model/Employee.java
@@ -0,0 +1,8 @@
+package ait.employee.model;
+
+public class Employee {
+    public int id;
+    public String name;
+    public double salary;
+    public boolean gender;
+}
