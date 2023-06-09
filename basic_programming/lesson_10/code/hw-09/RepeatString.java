b368f779ffc0d33e107c5fcba16e02fe931808e7
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-06-09 17:55:50 +0200

    lesson update

---
+++ basic_programming/lesson_10/code/hw-09/RepeatString.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class RepeatString {
+    public static void main(String[] args) {
+        System.out.println("Repeat the string");
+        //Задача 1
+//Программа получает на вход строку и число повторений этой строки
+//Программа должна вывести эту строку нужное количество раз.
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите строку: ");
+        String st = in.nextLine();
+        System.out.println("Введите желаемое количество повторений: ");
+        int n = in.nextInt();
+        int o = 0;
+        while (n > o) {
+            System.out.println(st);
+            o++;
+        }
+    }
+}
