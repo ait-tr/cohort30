9dbb45650ca7d752ff5a4028611c9a01ec4c032f
Leonid Kleimann <k_l_b_1963@hotmail.com>
2023-07-07 22:19:51 +0200

    lesson update

--- basic_programming/lesson_22/code/practice/user/UserAppl.java
+++ basic_programming/lesson_22/code/practice/user/UserAppl.java
@@ -5,6 +5,9 @@ import practice.user.model.User;
 public class UserAppl {
         public static void main(String[] args) {
 
+                User user1 = new User("username@mail.co", "Leonid19!");
+
+                System.out.println(user1);
 
         }
 }
