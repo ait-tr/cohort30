4775dd9ce04c48a468202703e89a703eb2b5387f
imiftakhov <ilias.miftakhov@yandex.ru>
2023-10-02 20:56:42 +0200

    Добавлен урок 58 - многопоточное программирование

--- basic_programming/lesson_58/src/teacher_code/TeacherCode2.java
+++ basic_programming/lesson_58/src/teacher_code/TeacherCode2.java
@@ -1,4 +1,48 @@
 package teacher_code;
 
 public class TeacherCode2 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("1  Thread + " + i);
+                    try {
+                        Thread.sleep(400);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 5; i++) {
+                    System.out.println("2  Thread + " + i);
+                    try {
+                        Thread.sleep(400);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("3  Main + " + i);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        System.out.println("========finish===========");
+    }
 }
