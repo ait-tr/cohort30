041ec6f70261cad3555a07794499d05c4bb8a287
imiftakhov <ilias.miftakhov@yandex.ru>
2023-11-01 21:06:07 +0100

    Добавлен урок 68 принципы ооп и solid

---
+++ basic_programming/lesson_68/src/teacher_code/NewGym.java
@@ -0,0 +1,14 @@
+package teacher_code;
+
+public class NewGym extends Gym {
+    @Override
+    public void takeCare() {
+        System.out.println("переопределенный метод");
+        System.out.println(getWeights());
+    }
+
+
+    public static void doExercise() {
+        System.out.println("переопределили статический метод");
+    }
+}
--- basic_programming/lesson_68/src/teacher_code/TeacherCode.java
+++ basic_programming/lesson_68/src/teacher_code/TeacherCode.java
@@ -2,27 +2,34 @@ package teacher_code;
 
 
 class TeacherCode {
-    private static String catelbell;
-    private static String weights;
-
-
-    private String towel;
-
-
     public static void main(String[] args) {
-        doSomething();
+        Gym ilyas = new Gym();
+        ilyas.setTowel("green towel");
+
+        Gym sandor = new Gym();
+        sandor.setTowel("blue towel");
+
+        sandor.setWeights("100 kg!");
+        sandor.doExercise();
+        sandor.takeCare();
 
 
+        ilyas.setWeights("60 kg!");
+        ilyas.doExercise();
+
+        sandor.doExercise();
+
+        ilyas.takeCare();
+
+
+        Gym.setWeights("20");
+
+        ilyas.doExercise();
+
+        Gym.doExercise();
+        new NewGym().doExercise();
     }
 
-    private static void doSomething() {
-        System.out.println("Я делаю упражнение на общем тренажере с весом " + weights);
-    }
-
-    private void takeCare() {
-        System.out.println("я взял небольшу паузу, чтобы передохнуть и вытереть лицо " + towel);
-        System.out.println(weights);
-    }
 
     private static class SomeClass {
         public static int x;
