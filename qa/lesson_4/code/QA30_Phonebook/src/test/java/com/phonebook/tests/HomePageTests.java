b640209007cf2e500751436da22f38011ca9b244
Ira Baker <49585685+irinabaker@users.noreply.github.com>
2024-02-02 22:13:15 +0100

    updated

---
+++ qa/lesson_4/code/QA30_Phonebook/src/test/java/com/phonebook/tests/HomePageTests.java
@@ -0,0 +1,18 @@
+package com.phonebook.tests;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HomePageTests extends TestBase{
+
+    @Test
+    public void isHomeComponentPresentTest() {
+        // System.out.println("Home component is " + isHomeComponentPresent());
+        Assert.assertTrue(isHomeComponentPresent());
+    }
+
+    public boolean isHomeComponentPresent() {
+        return driver.findElements(By.cssSelector("div:nth-child(2)>div>div>h1")).size()>0;
+    }
+}
---
+++ qa/lesson_4/plan.md
@@ -0,0 +1,10 @@
+## Задачи на урок:
+
+1. Exploratory testing. Test approaches(decomposition by pages/features/objects)
+2. Selenium. Method isElementPresent
+3. Assert testng 
+4. TestBase
+
+[Projects folder](https://drive.google.com/drive/folders/1KSR-UNrYhdzIPD8J9wJwP09aOcwYdR3s?usp=sharing)
+
+[Analysis](https://drive.google.com/drive/folders/16j8Kh59augC_yV1db-IRYhgr1bRY-oMf?usp=sharing)
